d7ea701132ecfa2c132e8a5d917b224411b14291
Nirmal Sarswat <nirmal@appsmith.com>
2024-05-27 18:18:40 +0530

    workflow updates

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -24,6 +24,11 @@ terraform {
       version = ">= 1.14.0"
     }
   }
+  backend "s3" {
+    bucket = "tf-state-docket"
+    key = "tag-service-terraform.tfstate"
+    region = "ap-south-1"
+  }
 
   required_version = "~> 1.3"
 }
