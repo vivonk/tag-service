45c7faca2e5102fc8495534ac726cc9189161c86
Nirmal Sarswat <nirmal@appsmith.com>
2024-05-28 22:04:52 +0530

    revert size add

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,18 +66,18 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 4
-      desired_size = 3
+      max_size     = 3
+      desired_size = 2
     }
 
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.2xlarge"]
+      instance_types = ["t3.xlarge"]
 
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      max_size     = 3
+      desired_size = 2
     }
   }
   tags = {
